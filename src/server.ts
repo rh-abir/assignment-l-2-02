5eb19e1039d09e286ce0bc08d7140539539eba44
rh-abir <rhabir.co@gmail.com>
2023-11-25 02:43:23 +0600

    setup file ready to run code

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './app'
+import config from './config'
+
+import mongoose from 'mongoose'
+
+async function server() {
+  try {
+    await mongoose.connect(config.db_url as string)
+
+    app.listen(config.port, () => {
+      console.log(`Server is runnign on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+server()
